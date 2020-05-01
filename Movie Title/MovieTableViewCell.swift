315ba0db68281d2611e0a23b9d2683d1f373134b
Alaa Adel <alaaadel@192.168.220.128>
2020-05-01 06:40:42 +0200

    First commit

--- Movie Title/MovieTableViewCell.swift
+++ Movie Title/MovieTableViewCell.swift
@@ -13,8 +13,10 @@ class MovieTableViewCell: UITableViewCell {
     @IBOutlet weak var movieTitleLabel: UILabel!
     @IBOutlet weak var movieYearLabel: UILabel!
     
+    @IBOutlet weak var moviesBackgroundView: UIView!
     override func awakeFromNib() {
         super.awakeFromNib()
+        cellDesign()
     }
 
     static let identifier = "MovieTableViewCell"
@@ -23,7 +25,12 @@ class MovieTableViewCell: UITableViewCell {
         return UINib(nibName: "MovieTableViewCell",
                      bundle: nil)
     }
-    
+    func cellDesign() {
+        self.moviesBackgroundView.layer.cornerRadius = 25
+        self.moviesBackgroundView.layer.borderWidth = 3
+        self.moviesBackgroundView.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        self.moviesBackgroundView.clipsToBounds = true
+    }
     func configure(with model: Movie) {
         self.movieTitleLabel.text = model.Title
         self.movieYearLabel.text = model.Year
